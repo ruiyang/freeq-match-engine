160534678b55918eaaa273ae05e838da0695df96
ryang <ryang.sei@gmail.com>
2015-03-14 23:22:20 +1100

    run match expression against dataset. minimal schema: {   "rule": string   "source-name": string   "source-id-name": string   "candidate-name": string   "candidate-id-name": string   "source": array   "candidate": array }
    example request:
    {
      "rule": "equal(boy.age, girl.age)",
      "source-name": "boy",
      "source-id-name": "name",
      "candidate-name": "girl",
      "candidate-id-name": "name",
      "source": [
        {"name": "James", "age": "20"},
        {"name": "John", "age": "25"}],
      "candidate": [
        {"name": "Mary1", "age": "21"},
        {"name": "Mary2", "age": "20"},
        {"name": "Mary3", "age": "25"},
        {"name": "Mary4", "age": "25"}]
    }
    
    response:
    {
        "result": [
            {
                "James": [
                    "Mary2"
                ]
            },
            {
                "John": [
                    "Mary3",
                    "Mary4"
                ]
            }
        ]
    }

--- src/freeq_match_engine/match_engine.clj
+++ src/freeq_match_engine/match_engine.clj
@@ -1,6 +1,28 @@
 (ns freeq-match-engine.match_engine
   (:require [clj-freeq.core :refer :all]))
 
-(defn run-rules [rule dataset]
-  (let [match-func (create-filter rule)]
-    (match-func dataset)))
+(defn- find-match-in-candidate [match-func source-name source-item candidate-name candidate-collection]
+  (let [source-key (keyword source-name)
+        candidate-key (keyword candidate-name)]
+    (filter #(match-func {source-key source-item candidate-key %1})
+            candidate-collection)))
+
+(defn- get-id [id-name col]
+  (let [id-keyword (keyword id-name)]
+    (map #(id-keyword %) col)))
+
+(defn run-rules [rule dataset source-name source-id-name candidate-name candidate-id-name]
+  (let [match-func (create-filter rule)
+        source-key (keyword source-name)
+        source-id-key (keyword source-id-name)
+        candidate-key (keyword candidate-name)
+        source (:source dataset)
+        candidate (:candidate dataset)]
+    (for [item source]
+      {(source-id-key item)
+       (get-id source-id-name
+               (find-match-in-candidate match-func
+                                        source-name
+                                        item
+                                        candidate-name
+                                        candidate))})))
--- src/freeq_match_engine/match_handler.clj
+++ src/freeq_match_engine/match_handler.clj
@@ -6,8 +6,19 @@
 (defn match [req]
   "run a matching rule against dataset"
   (let [rule (get-in req [:body :rule])
-        data (get-in req [:body :data])]
-    (resp/response {:result (run-rules rule data)})))
+        req-body (:body req)
+        source-name (get-in req [:body :source-name])
+        source-id-name (get-in req [:body :source-id-name])
+        candidate-name (get-in req [:body :candidate-name])
+        candidate-id-name (get-in req [:body :candidate-id-name])]
+    (resp/response
+     {:result
+      (run-rules rule
+                 req-body
+                 source-name
+                 source-id-name
+                 candidate-name
+                 candidate-id-name)})))
 
 (defroutes match-routes
   (POST "/matchit" [req] match))
