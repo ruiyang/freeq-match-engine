e380b52dccb55413aab69ee7301661424f311cdf
Rui Yang <ryang.sei@gmail.com>
2015-07-17 22:30:10 +1000

    upgrade to clojure 1.7 and other libraries

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
                  [ring/ring-json "0.3.1"]
-                 [ring-server "0.3.1"]
+                 [ring-server "0.4.0"]
                  [clj-freeq "0.0.1-SNAPSHOT"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler freeq-match-engine.handler/app}
